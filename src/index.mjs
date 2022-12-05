7727c9071cdc6391c5cebb9daddb4e4b4552ea13
7Kamil <115102676+7Kamil@users.noreply.github.com>
2022-12-06 02:47:37 +0400

    Create index.mjs

---
+++ src/index.mjs
@@ -0,0 +1,10 @@
+import View from "./View/view.mjs";
+import Model from "./Model/model.mjs";
+import Controller from "./Controller/controller.mjs";
+const init = () => {
+  const view = new View();
+  const model = new Model();
+  const controller = new Controller(model, view);
+  controller.init();
+};
+init();
